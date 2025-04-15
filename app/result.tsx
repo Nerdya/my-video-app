00b9e6b73f1a130002fffb45546f243953f5a7e9
cuongnk <cuongnk@3tit.vn>
2025-04-15 16:26:10 +0700

    Patch toIndex for different platforms

--- app/result.tsx
+++ app/result.tsx
@@ -1,15 +1,23 @@
 import { useRouter } from "expo-router";
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, Platform } from "react-native";
 
 export default function ResultScreen() {
   const router = useRouter();
+
+  const toIndex = () => {
+    if (Platform.OS === "android") {
+      router.back();
+    } else if (Platform.OS === "ios") {
+      router.push("/");
+    }
+  };
 
   return (
     <View style={{ padding: 20 }}>
       <Text style={{ marginBottom: 20, fontSize: 16 }}>
         This is the result screen. You can view the results here.
       </Text>
-      <Button title="Back to Index" onPress={() => router.back()} />
+      <Button title="Back to Index" onPress={toIndex} />
     </View>
   );
 }
