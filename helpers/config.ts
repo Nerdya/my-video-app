87c73de81677f38e4449e1c44daec4024f254c37
cuongnk <cuongnk@3tit.vn>
2025-05-12 13:42:24 +0700

    Update config.ts

--- helpers/config.ts
+++ helpers/config.ts
@@ -1,14 +1,12 @@
 export interface EnvConfig {
-  vpageBaseUrl: string;
-  vcoreBaseUrl: string;
-  appId: string;
+  vcoreBaseUrl?: string;
+  socketBaseUrl?: string;
+  appId?: string;
 }
 
 export const vkycTpcConfig = {
-  vpageBaseUrl: "https://vekyc-vpage-tpc-ui-uat.mobifi.vn",
   vcoreBaseUrl: "https://vekyc-gateway-server-uat.mobifi.vn",
   socketBaseUrl: "https://vekyc-vekyc-service-uat.mobifi.vn",
-  socketHealthCheck: 3000,
   appId: "b2d320ca642f48958f2b5e5cd1b1c547",
 };
 
